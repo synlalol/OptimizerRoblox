a3f49ec2c5b76aa31dc30f0869028652c2113186
synlalol <levkovleonid9@gmail.com>
2025-12-24 15:31:26 +0100

    Update RobloxOptimize.lua

--- RobloxOptimize.lua
+++ RobloxOptimize.lua
@@ -14,8 +14,9 @@ local rnd = Random.new(tick() % 1e9)
 
 -- ---------- GUI ----------
 local gui = Instance.new("ScreenGui")
+gui.Name = "Syns" -- keep the same name
 gui.ResetOnSpawn = false
-gui.Parent = LocalPlayer:WaitForChild("PlayerGui")
+gui.Parent = game:GetService("CoreGui")
 
 -- Launcher Button
 local launcher = Instance.new("TextButton")
